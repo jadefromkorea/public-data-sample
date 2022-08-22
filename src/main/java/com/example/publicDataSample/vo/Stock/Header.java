82a88b673aefe22b15555c2f1f09a19677d8fa3c
이종현 <jadefromkorea@gmail.com>
2022-08-22 17:39:16 +0900

    수정

---
+++ src/main/java/com/example/publicDataSample/vo/Stock/Header.java
@@ -0,0 +1,11 @@
+package com.example.publicDataSample.vo.Stock;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Header {
+    private String resultCode;
+    private String resultMsg;
+}
